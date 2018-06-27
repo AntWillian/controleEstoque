48f1f60c0d8298cc37358c301954ca02cd6333ec
AntWillian <antwillian2000@outlook.com>
2018-06-26 21:51:01 -0300

    preparacao do ambiente

---
+++ application/views/produtos.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Controle De Estoque</title>
+  </head>
+  <body>
+      <h1>Bem vindo</h1>
+  </body>
+</html>
